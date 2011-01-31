f9e1131b4760223befba27026bb93b0d13bd5e10
Timo Schilling <ts@demensys.de>
2011-01-31 21:19:57 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schilling::Application.config.secret_token = 'dc9a13857db0869c358e6d38e892ed76c8d2731f03b2f10cec306673aa3532c132b6da19cfa82e52aaca267398d3751e06e34c32341381cb1f2b920d8c070c6f'
